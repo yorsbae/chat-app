61aea60146e445432f65afda1fc071a8ac9da361
s4llm4n <s4llm4n@gmail.com>
2024-09-22 23:10:17 +0700

    leftbar

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,11 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import './Chat.css'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import ChatBox from '../../components/ChatBox/ChatBox'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
 
 const Chat = () => {
   return (
-    <div>
-      
+    <div className='chat'>
+      <div className='chat-container'>
+        <LeftSidebar/>
+        <ChatBox/>
+        <RightSidebar/>
+      </div>
     </div>
   )
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,25 +1,31 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
 
 const Login = () => {
+
+  const [currState,setCurrState] = useState('Sign up')  
+
   return (
     <div className='login'>
       <img src={assets.logo_big} alt='' className='logo'/>
       <form className='login-form'>
-        <h2>Sign Up</h2>
-        <input type="text" placeholder='username' className="form-input" required/>
-        <input type="email" placeholder='Email Address' className="form-input" />
-        <input type="password" placeholder='password' className="form-input" />
-        <button type='submit'>Sign Up</button>
+        <h2>{currState}</h2>
+        {currState === 'Sign up'?<input type="text" placeholder='username' className="form-input" required/>:null}
+        <input type="email" placeholder='Email Address' className="form-input" required/>
+        <input type="password" placeholder='password' className="form-input" required/>
+        <button type='submit'>{currState === 'Sign up'?'Create account':'Login now'}</button>
         <div className="login-term">
-          <input type="checkbox">
+          <input type="checkbox" />
           <p>Agree to the terms of use & privacy policy</p>
-          </input>
         </div>
         <div className="login-forgot">
-          <p className="login-toggle">Already have an account <span>Click here</span></p>
+          {
+            currState === 'Sign up'
+            ?<p className="login-toggle">Already have an account <span onClick={()=>setCurrState('Login')}>Login here</span></p>
+            :<p className="login-toggle">Create an account <span onClick={()=>setCurrState('Sign up')}>Click here</span></p>
+          }
         </div>
       </form>
     </div>
